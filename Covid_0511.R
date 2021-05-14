de02f3663af7dab60a627914a9959bc376d56195
cg657 <cg657@cornell.edu>
2021-05-13 21:53:06 -0400

    Revised

--- Covid_0511.R
+++ Covid_0511.R
@@ -1,7 +1,7 @@
+
+# install packages & library ----------------------------------------------
 library(tidyverse)
 library(dplyr)
-#install.packages("remotes")
-library(remotes)
 library(MASS)
 #install.packages("corrr")
 library(corrr)
@@ -10,16 +10,18 @@ library(corrplot)
 #install.packages("randomForest")
 #install.packages("Hmisc")
 library("Hmisc")
-
 #install.packages("gbm")
+
+
+
+# Clean data --------------------------------------------------------------
 
 #import dataset
 covid <- read_csv("~/R/Machine-Learning/data/COVID.csv")
 head(covid)
-
 summary(covid)
 
-#check how many samples have missing salary
+#check how many samples have missing values
 sum(is.na(covid))
 sum(is.na(covid$GDPCAP)) 
 sum(is.na(covid$POP)) 
@@ -33,7 +35,6 @@ sum(is.na(covid$HDI))
 
 covid = na.omit(covid)
 covid_clean = covid[,c('HDI','TC','TD','STI','GDPCAP')]
-  
 
 dim(covid_clean)
 sum(is.na(covid_clean))
@@ -49,10 +50,15 @@ cor_data
 # The p-value will be zero when it's very, very, very, very small, so small that R can't compute it anymore
 print(cor_data$P, digits = 20)
 
+
+
+
+# Method1&2: ridge regression and lasso regression ------------------------
+
+# GDPCAP as Dependent Variable 
 #ridge regression
 
 library(glmnet)
-
 
 # pass in an x matrix as well as a y vector
 
@@ -65,8 +71,6 @@ grid =10^seq (10,-2, length =100)
 # If alpha=0 then a ridge regression model is fit
 #ridge.mod =glmnet (x,y,alpha =0, lambda =grid)
 
-#split training set and testing set; 
-
 
 length <- 10
 mse_ridge <- rep(NA, length)
@@ -75,6 +79,7 @@ mse_lasso <- rep(NA, length)
 for (i in 1:10) {
   
   set.seed(i)
+  #split training set and testing set
   train  = sample (1: nrow(x), nrow(x)*0.7)
   test = (- train )
   y.test = y[test]
@@ -83,13 +88,13 @@ for (i in 1:10) {
   # fit a ridge regression model on the training set
   ridge.mod = glmnet(x[train,], y[train], alpha =0, lambda =grid)
   
-  #use cross-validation to choose the tuning parameter ??.
+  #use cross-validation to choose the tuning parameter.
   cv.out_ridge =cv.glmnet (x[train,],y[train],alpha =0)
   plot(cv.out_ridge)
   bestlam_ridge = cv.out_ridge$lambda.min
   bestlam_ridge # 0.09829758
   
-  # The test MSE associated with this value of ??.
+  # The test MSE associated with this value of.
   
   ridge.pred=predict(ridge.mod ,s = bestlam_ridge , newx = x[test,])
   mse_ridge[i] = mean((ridge.pred - y.test)^2)
@@ -102,13 +107,13 @@ for (i in 1:10) {
   # fit a lasso regression model on the training set
   lasso.mod = glmnet(x[train,], y[train], alpha = 1, lambda = grid)
   
-  #use cross-validation to choose the tuning parameter ??.
+  #use cross-validation to choose the tuning parameter.
   cv.out_lasso = cv.glmnet(x[train,],y[train],alpha = 1)
   plot(cv.out_lasso)
   bestlam_lasso = cv.out_lasso$lambda.min
   bestlam_lasso # 0.001006106
   
-  # The test MSE associated with this value of ??.
+  # The test MSE associated with this value of bestlam.
   
   lasso.pred=predict(lasso.mod, s = bestlam_lasso , newx = x[test,])
   mse_lasso[i] = mean((lasso.pred - y.test)^2) #2.121526
@@ -116,7 +121,7 @@ for (i in 1:10) {
 }
 
 
-# we refit our ridge regression model on the full data set, using the value of ?? chosen by cross-validation, and examine the coefficient estimates.
+# we refit our ridge regression model on the full data set, using the value of bestlam chosen by cross-validation, and examine the coefficient estimates.
 
 ridge.out = glmnet(x,y,alpha =0, lambda = bestlam_ridge)
 coef(ridge.out)[,1]
@@ -127,14 +132,14 @@ coef(lasso.out)[,1]
 lasso.coef=coef(lasso.out)[,1]
 lasso.coef[lasso.coef!=0]
 summary(lasso.out)
-?????????????????????refit
 
 
 mean_mse_ridge = mean(mse_ridge)
 mean_mse_lasso = mean(mse_lasso)
 
 
-# HDI as Dependent Variable -----------------------------------------------
+# HDI as Dependent Variable 
+#ridge
 
 # pass in an x matrix as well as a y vector
 
@@ -162,9 +167,9 @@ for (i in 1:10) {
   cv.out2_ridge =cv.glmnet (x[train2,],y[train2],alpha =0)
   plot(cv.out2_ridge)
   bestlam2_ridge = cv.out2_ridge$lambda.min
-  bestlam2_ridge 
+  bestlam2_ridge #0.009006384
   
-  # The test MSE associated with this value of ??.
+  # The test MSE associated with this value of bestlam.
   
   ridge.pred2=predict(ridge.mod2 ,s = bestlam2_ridge , newx = x[test2,])
   mse2_ridge[i] = mean((ridge.pred2 - y.test2)^2)
@@ -179,9 +184,9 @@ for (i in 1:10) {
   cv.out2_lasso = cv.glmnet(x[train2,],y[train2],alpha = 1)
   plot(cv.out2_lasso)
   bestlam2_lasso = cv.out2_lasso$lambda.min
-  bestlam2_lasso 
+  bestlam2_lasso #0.0001110349
   
-  # The test MSE associated with this value of ??.
+  # The test MSE associated with this value of bestlam.
   
   lasso.pred2=predict(lasso.mod2, s = bestlam2_lasso , newx = x[test2,])
   mse2_lasso[i] = mean((lasso.pred2 - y.test2)^2)
@@ -193,7 +198,7 @@ mean_mse2_lasso = mean(mse2_lasso)
 
 
 
-# we refit our ridge regression model on the full data set, using the value of ?? chosen by cross-validation, and examine the coefficient estimates.
+# we refit our lasso regression model on the full data set, using the value of bestlam chosen by cross-validation, and examine the coefficient estimates.
 
 ridge2.out = glmnet(x,y,alpha =0, lambda = bestlam2_ridge)
 coef(ridge2.out)[,1]
@@ -204,62 +209,57 @@ coef(lasso2.out)[,1]
 lasso2.coef=coef(lasso2.out)[,1]
 lasso2.coef[lasso.coef!=0]
 summary(lasso2.out)
-?????????????????????refit
 
 
 
-SSE <- sum((predicted - true)^2)
-SST <- sum((true - mean(true))^2)
-R_square <- 1 - SSE / SST
-RMSE = sqrt(SSE/nrow(df))
+# Method3&4: regression tree: bagging and boosting ------------------------
 
-
-
-
-# Regression tree ---------------------------------------------------------
 # regression tree, bagging
 library (randomForest)
 
 # GDPCAP as dependent variable
 length <- 10
-mse_tree <- rep(NA, length)
-for (i in 1:2) {
+mse_bag <- rep(NA, length)
+for (i in 1:10) {
 #split data into training set and testing set
-tree_train = sample(dim(covid_clean)[1],dim(covid_clean)[1]*0.7)
-tree.train = covid_clean[tree_train, ]
-tree.test = covid_clean[-tree_train,]
+bag_train = sample(dim(covid_clean)[1],dim(covid_clean)[1]*0.7)
+bag.train = covid_clean[bag_train, ]
+bag.test = covid_clean[-bag_train,]
 
-bag.covid = randomForest(GDPCAP~.,data=tree.train, mtry=4, importance =TRUE)
-bag.covid
-importance(bag.covid)
+bag.GDP = randomForest(GDPCAP~.,data = bag.train, mtry=4, importance =TRUE)
+bag.GDP
+importance(bag.GDP)
 
-yhat.bag = predict(bag.covid,newdata = tree.test)
-mse_tree[i] = mean((yhat.bag - tree.test$GDPCAP)^2)
+yhat.bag = predict(bag.GDP,newdata = bag.test)
+mse_bag[i] = mean((yhat.bag - bag.test$GDPCAP)^2)
 }
 
-mse_tree
+
+mean_mse_bag= mean(mse_bag)
+
 
 # HDI as dependent variable
 length <- 10
-mse_tree2 <- rep(NA, length)
+mse_bag2 <- rep(NA, length)
+
 for (i in 1:2) {
   #split data into training set and testing set
-  tree2_train = sample(dim(covid_clean)[1],dim(covid_clean)[1]*0.7)
-  tree2.train = covid_clean[tree2_train, ]
-  tree2.test = covid_clean[-tree2_train,]
+  bag2_train = sample(dim(covid_clean)[1],dim(covid_clean)[1]*0.7)
+  bag2.train = covid_clean[bag2_train, ]
+  bag2.test = covid_clean[-bag2_train,]
   
-  bag.HDI = randomForest(HDI~.,data=tree2.train, mtry=4, importance =TRUE)
+  bag.HDI = randomForest(HDI~.,data=bag2.train, mtry=4, importance =TRUE)
   bag.HDI
   importance(bag.HDI)
   
-  yhat.bag2 = predict(bag.HDI,newdata = tree2.test)
-  mse_tree2[i] = mean((yhat.bag2 - tree2.test$HDI)^2)
+  yhat.bag2 = predict(bag.HDI, newdata =bag2.test)
+  mse_bag2[i] = mean((yhat.bag2 - bag2.test$HDI)^2)
 }
 
-mse_tree2
+mean_mse_bag2 = mean(mse_bag2)
 
 
-# Boosting ----------------------------------------------------------------
+# regression tree, Boosting 
 library (gbm)
 
 # run a basic GBM model, GDPCAP as response
@@ -316,7 +316,7 @@ mean_mse_boosting = mean(mse_boosting)
 summary(boost.GDP)
 par(mfrow =c(1,2))
 plot(boost.GDP ,i="HDI")
-plot(boost.GDP ,i="TC")
+plot(boost.GDP ,i="STI")
 
 
 
